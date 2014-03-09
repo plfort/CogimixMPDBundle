fc28cefb0bbdaace74e751a40d8115376d7e4700
plfort <plfort@cogipix.com>
2014-03-09 23:31:09 +0100

    new menu

--- ViewHooks/Menu/MenuItem.php
+++ ViewHooks/Menu/MenuItem.php
@@ -1,17 +1,22 @@
 <?php
 namespace Cogipix\CogimixMPDBundle\ViewHooks\Menu;
 use Cogipix\CogimixCommonBundle\ViewHooks\Menu\MenuItemInterface;
+use Cogipix\CogimixCommonBundle\ViewHooks\Menu\AbstractMenuItem;
 
 /**
  *
  * @author plfort - Cogipix
  *
  */
-class MenuRenderer implements MenuItemInterface{
+class MenuItem  extends AbstractMenuItem{
 
     public function getMenuItemTemplate()
     {
           return 'CogimixMPDBundle:Menu:menu.html.twig';
 
+    }
+
+    public function getName(){
+    	return 'mpd';
     }
 }
